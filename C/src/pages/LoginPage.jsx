80aba40898e2e27bf24a4087334b1866571b22ef
Shiitakke <matthewstepfner@gmail.com>
2025-11-15 11:43:08 +1100

    update style for responsiveness

--- C/src/pages/LoginPage.jsx
+++ C/src/pages/LoginPage.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { redirect, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 
 const Loginpage = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const inputFieldStyle = "m-5 bg-white border-b rounded-t-md shadow-md";
+  const inputFieldStyle = "w-full h-10 px-2 bg-white border-b rounded-t-md shadow-md mt-2 mb-5";
 
   const attemptLogin = (email, password) => {
     if (email.length === 0) {
@@ -32,25 +32,34 @@ const Loginpage = () => {
       return;
     }
     // Everything correct, redirect to dashboard
+    localStorage.setItem("loggedInUser", email);
     navigate("/dashboard");
   }
 
   return (
   <> 
   {/* Container for entire form */}
-  <div className="w-120 h-140 m-auto relative top-50">
+  <div 
+    className="w-screen h-screen flex"
+  >
     {/* Container for form */}
-    <div className="w-auto h-full p-[10%] bg-stone-100 rounded-4xl shadow-[0px_0px_10px_grey]">
+    <div 
+      className="shadow-[0px_0px_10px_grey] w-screen h-screen min-w-60 min-h-70 sm:w-100 sm:h-100 sm:max-h-[95vh] sm:m-auto sm:rounded-4xl sm:shadow-[0px_0px_10px_grey] p-10 bg-stone-100"
+    >
       {/* Email field */}
       <div>
-        <label htmlFor="emailInput">Email:</label>
-        <input
-          className={inputFieldStyle}
-          type="text"
-          id="emailInput"
-          name="emailInput"
-          onChange={(e) => {setEmail(e.target.value)}}
-        />
+        <div>Email:</div>
+        <div>
+          <input
+            className={inputFieldStyle}
+            type="text"
+            id="emailInput"
+            name="emailInput"
+            placeholder="Enter email"
+            onChange={(e) => {setEmail(e.target.value)}}
+          />
+        </div>
+
       </div>
       {/* Password field */}
       <div>
@@ -60,12 +69,13 @@ const Loginpage = () => {
           type="password"
           id="passwordInput"
           name="passwordInput"
+          placeholder="Enter password"
           onChange={(e) => {setPassword(e.target.value)}}
-        />        
+        />
       </div>
       {/* Login button */}
       <input 
-        className="px-[2%] py-[1%] bg-white float-right rounded-md shadow-md hover:cursor-pointer hover:shadow-lg hover:bg-stone-200"
+        className="px-[2%] py-[1%] bg-white border-b float-right rounded-t-md shadow-md hover:cursor-pointer hover:shadow-lg hover:bg-stone-200"
         type="submit"
         name="loginButton"
         value="Login"
